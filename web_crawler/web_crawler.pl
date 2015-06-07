689118e9dc6486ff894bb0333574c6a475f588e8
pvsukalkar <pvsukalkar@iitj.ac.in>
2015-06-07 12:45:32 +0530

    simple web crawler

---
+++ web_crawler/web_crawler.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+
+#A simple web crawler to demonstrate LWP::Simple and perl 
+use strict; 
+use LWP::Simple; 
+
+#my $url = shift @ARGV || die "";
+my $url = shift || die "Please provide initial url ";
+my $max = 10 ; 
+
+my $html = get($url); 
+my@urls; 
+
+while ($html =~ s/(http:\/\/\S+)[">]//) {
+	push @urls, $1; 
+} 
+
+mkdir ("web",0755);
+my $weburl="web/url.map";
+open(URLMAP,">$weburl") || die "can't open url.map";
+
+my $count=0;
+print "urls size: ".$#urls;
+for (my $i=0; $i<$max ; $i++) {
+	my $source = $urls[int(rand ($#urls+1))];
+	getstore($source,"web/$count.html");
+	print URLMAP "$count\t$source\n";
+	print STDERR "Getting $count: $source\n";
+	$count++;
+}
+
+close URLMAP; 
+
