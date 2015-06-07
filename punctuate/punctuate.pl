e9a979fbcf604cc31f7f524848084d410b72c9e7
pvsukalkar <pvsukalkar@iitj.ac.in>
2015-06-07 12:25:56 +0530

    punctuation program
    punctuates any english text file.
    Learning regex 1

---
+++ punctuate/punctuate.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use strict;
+my $filename = "english.txt";
+my $tempfile = "english.txt.tmp";
+open (FILE,'<',$filename) || die "could not open english.txt";
+open (FILE2,">$tempfile") || die "could not open english.txt.tmp";
+my $sentenceFinished = 0;
+my $lineno=0;
+while (my $row = <FILE>){
+	chomp $row; 
+	#$row =~ s/\s+//g;
+	if($sentenceFinished == 1 || $lineno == 0){
+		$row =~ s/(\w)(.*)/uc($1).$2/eg;
+	}
+	$row =~ s/(\w+\s*[!.?]+\s*)(\w)(\w+)/$1.uc($2).$3/eg;
+	print FILE2 $row."\n";
+	print $row."\n";
+	if( $row =~ m/[.?!]$/){
+		$sentenceFinished=1;
+	}
+	else{
+		$sentenceFinished=0;
+	}
+	$lineno++;
+}
+close FILE;
+close FILE2; 
